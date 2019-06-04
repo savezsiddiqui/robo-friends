2ffb7c9590104f35c2e9134caff58cd486545f82
savezsiddiqui <savezsiddiqui@gmail.com>
2019-06-05 00:09:28 +0530

    Updates

---
+++ precache-manifest.146ba112cc41d5dd102007230e88b0ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "c51058804c8254bbd51e",
+    "url": "/robo-friends/static/js/main.c5105880.chunk.js"
+  },
+  {
+    "revision": "de27281107cf17b27160",
+    "url": "/robo-friends/static/js/1.de272811.chunk.js"
+  },
+  {
+    "revision": "c51058804c8254bbd51e",
+    "url": "/robo-friends/static/css/main.7dcbbad6.chunk.css"
+  },
+  {
+    "revision": "de27281107cf17b27160",
+    "url": "/robo-friends/static/css/1.b268eadf.chunk.css"
+  },
+  {
+    "revision": "550a2919e81c385f6c6b1583e28f5e57",
+    "url": "/robo-friends/index.html"
+  }
+];
